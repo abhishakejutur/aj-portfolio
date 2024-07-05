d07cf0e24557a49962131b3f824a8c4f8b0978e8
Abhishake Jutur <91953148+abhishakejutur@users.noreply.github.com>
2024-07-05 09:34:51 +0530

    Update layout.jsx
    Adding meta keywords, author, description..

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { JetBrains_Mono } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 //components
@@ -6,7 +7,7 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 
-const jetBrainsmono = JetBrains_Mono({ 
+const jetBrainsmono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   variable: "--font-jetbrainsmono"
@@ -20,12 +21,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content="portfolio, Jutur Abhishake, web developer, projects, abhi, abhishek, software developer, software, ASP.NET, SQL, sql, engineer" />
+        <meta name="author" content="Jutur Abhishake" />
+      </Head>
       <body className={jetBrainsmono.variable}>
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
-        </body>
+      </body>
     </html>
   );
 }
-
