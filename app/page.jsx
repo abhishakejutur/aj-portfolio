e4434439560e1214b1de6f39b0deb128c252421f
Abhishake Jutur <91953148+abhishakejutur@users.noreply.github.com>
2024-07-17 12:05:00 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Button } from "@/components/ui/button"
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
 // components
@@ -8,6 +10,11 @@ import { Photo } from "@/components/Photo";
 import Stats from "@/components/Stats";
 
 const Home = () => {
+  const playClickSound = () => {
+    const audio = new Audio("/assets/click1.mp3");
+    audio.play();
+  };
+
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
@@ -18,17 +25,21 @@ const Home = () => {
               Hello I'm <br /> <span className="text-accent">Jutur Abhishake</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I specialize in writing APIs using ASP.NET and have solid skills in database management, PostgreSQL and SQL servers. I am also proficient in various programming languages and technologies.
+              I specialize in writing APIs using ASP.NET and have solid skills in database management and SQL servers. I am also proficient in various programming languages and technologies.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <a href="/assets/Resume/JuturAbhishake.pdf" download className="uppercase flex items-center gap-2">
+              <a href="/assets/Resume/JuturAbhishake.pdf" download className="uppercase flex items-center gap-2" onClick={playClickSound}>
                 <Button variant="outline" size="lg">
                   <span>Download CV</span>
                   <FiDownload className="text-xl ml-2" />
                 </Button>
               </a>
               <div className="mb-8 xl:mb-8"><br />
-                <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+                <Socials 
+                  containerStyles="flex gap-6" 
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" 
+                  playClickSound={playClickSound} // Pass the sound function to Socials component
+                />
               </div>
             </div>
           </div>
@@ -42,4 +53,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
