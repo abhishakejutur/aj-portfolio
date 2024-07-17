52b06b585624fec6a320aa3ca1de0db67904c81e
Abhishake Jutur <91953148+abhishakejutur@users.noreply.github.com>
2024-07-17 12:04:05 +0530

    Update page.jsx

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,6 +1,8 @@
 "use client";
+import { useState } from 'react';
 import { BsArrowDownRight } from "react-icons/bs";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const services = [
   {
@@ -28,9 +30,13 @@ const services = [
     href: "https://github.com/abhishakejutur/call_logs_access_app_6"
   }
 ];
-import { motion } from "framer-motion";
 
 const Services = () => {
+  const playClickSound = () => {
+    const audio = new Audio("/assets/click1.mp3");
+    audio.play();
+  };
+
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
       <div className="container mx-auto">
@@ -42,17 +48,17 @@ const Services = () => {
           }}
           className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
         >
-          {services.map((services, index)=>{
+          {services.map((service, index) => {
             return (
               <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
                 <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{services.num}</div>
-                  <Link href={services.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
+                  <Link href={service.href} onClick={playClickSound} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
                     <BsArrowDownRight className="text-primary text-3xl"/>
                   </Link>
                 </div>
-                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{services.title}</h2>
-                <p className="text-white/60">{services.description}</p>
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
+                <p className="text-white/60">{service.description}</p>
                 <div className="border-b border-white/20 w-full"></div>
               </div>
             );
@@ -63,4 +69,4 @@ const Services = () => {
   )
 }
 
-export default Services
+export default Services;
