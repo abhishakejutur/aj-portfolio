6f6fea1284d11bc3df474c81cfb1ca4b4c52478f
Abhishake Jutur <91953148+abhishakejutur@users.noreply.github.com>
2024-09-15 22:21:36 +0530

    Update sendEmail.js

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -8,19 +8,23 @@ export default async function handler(req, res) {
     let transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: 'abhishake62232@gmail.com', 
-        pass: 'lyzo pqby duwc xbvi', 
+        user: 'abhishake62232@gmail.com',
+        pass: 'lyzo pqby duwc xbvi',
       },
     });
 
+    // Admin notification email
     let mailOptions = {
       from: 'abhishake62232@gmail.com',
-      to: 'abhishake62232@gmail.com', 
-      subject: `AJ-Portfolio - New message from ${firstname} ${lastname}`,
+      to: 'abhishake62232@gmail.com',
+      subject: `AJ-Portfolio - New notification from ${firstname} ${lastname}`,
       html: `
         <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;">
           <div style="background-color: white; border-radius: 8px; padding: 20px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
             <h2 style="color: #333;">Hey Abhi! You got a new message from ${firstname} ${lastname}.</h2>
+            <div style="text-align: center; margin-bottom: 20px;">
+              <img src="https://drive.google.com/uc?id=1Onh-LJdr7gl1VzfGet_lGogBAAB_lkpj" style="max-width: 50%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); margin-top: 20px;" alt="Profile Image"/>
+            </div>
             <p style="color: #555;">The user details are below:</p>
             <table style="border-collapse: collapse; width: 100%; font-family: Arial, sans-serif;">
               <tr style="background-color: #f2f2f2;">
@@ -43,24 +47,28 @@ export default async function handler(req, res) {
                 <td style="border: 1px solid #ddd; padding: 8px;">Service</td>
                 <td style="border: 1px solid #ddd; padding: 8px;">${service}</td>
               </tr>
-              <tr>
-                <td style="border: 1px solid #ddd; padding: 8px;">Message</td>
-                <td style="border: 1px solid #ddd; padding: 8px;">${message}</td>
-              </tr>
             </table>
+            <div style="background-color: #f2f2f2; margin-top: 20px; padding: 15px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+              <h3 style="margin-bottom: 10px; color: #333;">Message:</h3>
+              <p style="color: #555; line-height: 1.6;">${message}</p>
+            </div>
           </div>
         </div>
       `,
     };
 
+    // Auto-reply email for the user
     let autoReplyOptions = {
       from: 'abhishake62232@gmail.com',
-      to: email, 
+      to: email,
       subject: `Hey ${firstname}, Thank you for visiting my portfolio!`,
       html: `
         <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px;">
           <div style="background-color: white; border-radius: 8px; padding: 20px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
             <h2 style="color: #333;">Hey ${firstname} ${lastname}!</h2><br />
+            <div style="text-align: center; margin-bottom: 20px;">
+              <img src="https://drive.google.com/uc?id=1Onh-LJdr7gl1VzfGet_lGogBAAB_lkpj" style="max-width: 50%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); margin-top: 20px;" alt="Profile Image"/>
+            </div>
             <p style="color: #555;">
               This is Jutur Abhishake. I hope this email finds you well. Thank you for visiting my portfolio and expressing interest in my ${service} services.
             </p>
@@ -76,20 +84,47 @@ export default async function handler(req, res) {
               <li>PostgreSQL</li>
               <li>SQL Server</li>
             </ul>
-            <p style="color: #555;">
-              You can connect with me or explore my portfolio through the following links:
-            </p>
+            <p style="color: #555;">Your details are as follows:</p>
+            <table style="border-collapse: collapse; width: 100%; font-family: Arial, sans-serif;">
+              <tr style="background-color: #f2f2f2;">
+                <th style="border: 1px solid #ddd; padding: 8px; text-align: left;">Field</th>
+                <th style="border: 1px solid #ddd; padding: 8px; text-align: left;">Details</th>
+              </tr>
+              <tr>
+                <td style="border: 1px solid #ddd; padding: 8px;">Name</td>
+                <td style="border: 1px solid #ddd; padding: 8px;">${firstname} ${lastname}</td>
+              </tr>
+              <tr>
+                <td style="border: 1px solid #ddd; padding: 8px;">Email</td>
+                <td style="border: 1px solid #ddd; padding: 8px;">${email}</td>
+              </tr>
+              <tr>
+                <td style="border: 1px solid #ddd; padding: 8px;">Phone Number</td>
+                <td style="border: 1px solid #ddd; padding: 8px;">${phone}</td>
+              </tr>
+              <tr>
+                <td style="border: 1px solid #ddd; padding: 8px;">Service</td>
+                <td style="border: 1px solid #ddd; padding: 8px;">${service}</td>
+              </tr>
+            </table>
+            <div style="background-color: #f2f2f2; margin-top: 20px; padding: 15px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+              <h3 style="margin-bottom: 10px; color: #333;">Message:</h3>
+              <p style="color: #555; line-height: 1.6;">${message}</p>
+            </div>
+            <p style="color: #555;">You can connect with me or explore my portfolio through the following links:</p>
             <div style="text-align: center; margin-top: 20px;">
-              <a href="https://in.linkedin.com/in/abhishakejutur" style="text-decoration: none;">
-                <button style="background-color: #0073b1; border: none; color: white; padding: 12px 24px; font-size: 16px; cursor: pointer; border-radius: 8px; margin-right: 10px; transition: background-color 0.3s ease;">
-                  LinkedIn Profile
-                </button>
-              </a>
-              <a href="https://aj-portfolio-five.vercel.app" style="text-decoration: none;">
-                <button style="background-color: #28a745; border: none; color: white; padding: 12px 24px; font-size: 16px; cursor: pointer; border-radius: 8px; margin-left: 10px; transition: background-color 0.3s ease;">
-                  Visit My Portfolio
-                </button>
-              </a>
+              <div>
+                <a href="https://in.linkedin.com/in/abhishakejutur" style="text-decoration: none; width: 50%;">
+                  <button style="background-color: #0073b1; border: none; color: white; padding: 12px 24px; font-size: 16px; cursor: pointer; border-radius: 8px; width: 100%; transition: background-color 0.3s ease;">
+                    LinkedIn Profile
+                  </button>
+                </a><br /><br />
+                <a href="https://aj-portfolio-five.vercel.app" style="text-decoration: none; width: 50%;">
+                  <button style="background-color: #28a745; border: none; color: white; padding: 12px 24px; font-size: 16px; cursor: pointer; border-radius: 8px; width: 100%; transition: background-color 0.3s ease;">
+                    Visit My Portfolio
+                  </button>
+                </a>
+              </div>
             </div>
             <p style="margin-top: 20px; color: #555;">Best regards,</p>
             <p style="color: #555;">Jutur Abhishake</p>
@@ -100,10 +135,21 @@ export default async function handler(req, res) {
         <style>
           a button:hover {
             background-color: #005b8e !important;
-            cursor: pointer;
           }
           a button:nth-child(2):hover {
             background-color: #218838 !important;
+          }
+          @media only screen and (max-width: 600px) {
+            .flex {
+              flex-direction: column !important;
+            }
+            a {
+              width: 100% !important;
+            }
+            a button {
+              width: 100% !important;
+              margin-bottom: 10px;
+            }
           }
         </style>
       `,
@@ -121,4 +167,3 @@ export default async function handler(req, res) {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
-//<img src="https://drive.google.com/file/d/1Onh-LJdr7gl1VzfGet_lGogBAAB_lkpj/view?usp=sharing" alt="JA" style="width: 150px; height: auto; border-radius: 50%; margin: 20px auto;" /><br />
